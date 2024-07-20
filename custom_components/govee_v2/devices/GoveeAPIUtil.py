5a7304ff4f6f071b5ba800fbfdd8996d664d527d
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-07-20 11:15:21 -0500

    Improve logging

--- custom_components/govee_v2/devices/GoveeAPIUtil.py
+++ custom_components/govee_v2/devices/GoveeAPIUtil.py
@@ -9,22 +9,25 @@ BASE_URL = "https://openapi.api.govee.com"
 
 
 async def get_device_state(api_key: str, sku: str, device: str):
-    headers = {'Govee-API-Key': api_key, 'Content-Type': 'application/json'}
-    body = {"requestId": "uuid", "payload": {"sku": sku, "device": device}}
+    try:
+        headers = {'Govee-API-Key': api_key, 'Content-Type': 'application/json'}
+        body = {"requestId": "uuid", "payload": {"sku": sku, "device": device}}
 
-    async with httpx.AsyncClient() as client:
+        async with httpx.AsyncClient() as client:
 
-        response = await client.post(f"{BASE_URL}/router/api/v1/device/state", headers=headers, json=body)
-        log.warning(f"Failed to get device state: {response.status_code} - {response.text}")
+            response = await client.post(f"{BASE_URL}/router/api/v1/device/state", headers=headers, json=body)
 
-    if response.status_code == 200:
-        if "payload" in response.json():
-            payload = response.json()["payload"]
-            if "capabilities" in payload:
-                return payload["capabilities"]
+        if response.status_code == 200:
+            if "payload" in response.json():
+                payload = response.json()["payload"]
+                if "capabilities" in payload:
+                    return payload["capabilities"]
 
-    else:
-        log.error(f"Failed to get device state: {response.status_code} - {response.text}")
+        else:
+            log.warning(f"Failed to get device state: {response.status_code} - {response.text}")
+    except Exception as e:
+        log.error(f"Failed to get device state: {e}")
+        raise e
 
 
 async def control_device(api_key: str, sku: str, device: str, capability: dict) -> bool:
